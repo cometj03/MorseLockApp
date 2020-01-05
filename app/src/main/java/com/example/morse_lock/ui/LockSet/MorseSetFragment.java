a5a79477e1f24b6e43e67ab64144580a70b75b81
XxCtrlZxX <harryjung1001@gmail.com>
2020-01-05 23:35:52 +0900

    SeekBar
    SharedPreferences 에 putInt, putBoolean 넣음

--- app/src/main/java/com/example/morse_lock/ui/LockSet/MorseSetFragment.java
+++ app/src/main/java/com/example/morse_lock/ui/LockSet/MorseSetFragment.java
@@ -1,7 +1,5 @@
 package com.example.morse_lock.ui.LockSet;
 
-import android.app.AlertDialog;
-import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.graphics.Color;
@@ -10,8 +8,10 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.SeekBar;
 import android.widget.Switch;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
@@ -25,17 +25,50 @@ public class MorseSetFragment extends Fragment {
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
+
         SharedPreferences pref = this.getActivity().getSharedPreferences("LOCK", 0);
+        SharedPreferences.Editor editor = pref.edit();
         boolean[] isLocked = {false};
         isLocked[0] = pref.getBoolean("isLocked", false);
-        morseSetViewModel =
-                ViewModelProviders.of(this).get(MorseSetViewModel.class);
+        int[] sensitivity = {pref.getInt("sensitivity", 0)};
+        if (sensitivity[0] == 0)
+            sensitivity[0] = 50;
+
+        morseSetViewModel = ViewModelProviders.of(this).get(MorseSetViewModel.class);
         View root = inflater.inflate(R.layout.fragment_morse_set, container, false);
+
         Button changeBtn = root.findViewById(R.id.changeBtn);
         Switch switch1 = root.findViewById(R.id.switch1);
+        SeekBar bar_sensitivity = root.findViewById(R.id.bar_sensitivity);
+        TextView txt_sensitivity = root.findViewById(R.id.txt_sensitivity);
+
+        bar_sensitivity.setProgress(sensitivity[0]);
+        txt_sensitivity.setText((double)sensitivity[0] / 200.0 + "s");
 
         Intent myIntent = new Intent(getActivity(), MorseSetActivity.class);
 
+        // 감도 조절 SeekBar
+        bar_sensitivity.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
+                sensitivity[0] = i;
+                txt_sensitivity.setText((double)i / 200.0 + "s");
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+                editor.putBoolean("isSensitivity", true);
+                editor.putInt("sensitivity", sensitivity[0]);
+                editor.commit();
+            }
+        });
+
+        // 버튼 비활성화 or 활성화
         changeBtn.setEnabled(isLocked[0]);
         switch1.setChecked(isLocked[0]);
         if (isLocked[0]){
@@ -51,6 +84,7 @@ public class MorseSetFragment extends Fragment {
                 myIntent.putExtra("title", "change morse");
                 startActivity(myIntent);
             }));
+
         morseSetViewModel.getText().observe(this, s -> switch1.setOnCheckedChangeListener((compoundButton, b) -> {
             if (b)
             {
@@ -59,19 +93,12 @@ public class MorseSetFragment extends Fragment {
                 myIntent.putExtra("title", "set morse");
                 startActivity(myIntent);
             }else{
-                AlertDialog.Builder builder = new AlertDialog.Builder(getActivity(), android.R.style.Theme_Material_Light_Dialog_Alert);   // 알림창
-                builder.setTitle("알림").setMessage("잠금을 해제하시겠습니까?");
-                builder.setPositiveButton("확인", (dialog, which) -> {    // 확인 버튼
-                    switch1.setTextColor(Color.parseColor("#9b9b9b"));
-                    switch1.setText("UnLocked");
-                    changeBtn.setEnabled(false);
-                    SharedPreferences.Editor editor = pref.edit();
-                    editor.putBoolean("isLocked", false);
-                    editor.commit();
-                });
-                builder.setNegativeButton("취소", ((dialog, which) -> isLocked[0] = true));
-                AlertDialog alertDialog = builder.create();
-                alertDialog.show();
+                Toast.makeText(getActivity(), "잠금 설정을 해제했습니다.", Toast.LENGTH_SHORT).show();
+                switch1.setTextColor(Color.parseColor("#9b9b9b"));
+                switch1.setText("UnLocked");
+                changeBtn.setEnabled(false);
+                editor.putBoolean("isLocked", false);
+                editor.commit();
             }
         }));
         return root;
